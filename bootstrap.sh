3fe35c0a672330b301990f51a6bab080165efec3
d4rkb4sh8 <d4rkb4sh8@host>
2024-05-27 07:52:27 -0600

    modified bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,48 @@
 #!/usr/bin/env  bash
 
-# Configuration
-CONFIG_DIR="$HOME/.dotfiles/.config"
-APT_PACKAGES="stow lynis gawk curl wget git alacritty powerline* nala fastfetch net-tools forensics-all cpufetch btop gnome-shell-extension-manager flatpak gnome-software-plugin-flatpak gh git lolcat fd-find sd npm vlc build-essential procps file net-tools httpie mitmproxy gpaste-2 font-manager gdebi ufw gawk cmake plocate bat most libssl-dev libvips-dev libsixel-dev libchafa-dev libtbb-dev"
-HOMEBREW_PACKAGES="gcc dust dog eza zellij neovim xh yazi ffmpegthumbnailer unar jq poppler fd ripgrep zoxide"
+# CONFIGURATION
+
+# Function to remove bloatware
+remove_bloat() {
+	sudo apt purge gnome-games transmission libreoffice* -y && sudo apt autoremove -y && sudo apt autoclean -y
+}
+
+$remove_bloat
+
 
 # Function to install apt packages
+APT_PACKAGES="stow figlet lynis gawk curl wget git alacritty powerline* nala fastfetch net-tools forensics-all cpufetch btop gnome-shell-extension-manager flatpak gnome-software-plugin-flatpak gh git lolcat fd-find sd npm vlc build-essential procps file net-tools httpie mitmproxy gpaste-2 font-manager gdebi ufw gawk cmake plocate bat most libssl-dev libvips-dev libsixel-dev libchafa-dev libtbb-dev"
+
 install_apt_packages() {
-	sudo apt update
-	sudo apt install -y "$APT_PACKAGES"
+	sudo apt update -y && sudo apt upgrade -y 
+	sudo apt install -y $APT_PACKAGES
 }
 
-# Function to install Homebrew packages
-install_homebrew_packages() {
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>$HOME/.bashrc
-	brew install $HOMEBREW_PACKAGES
-}
+$install_apt_packages
+
+
+
 # Function to install oh-my-bash
 install_oh_my_bash() {
 	bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 }
+
+$install_oh_my_bash
+
+
+
+
+# Function to install Homebrew packages
+HOMEBREW_PACKAGES="gcc dust dog eza zellij neovim xh yazi ffmpegthumbnailer unar jq poppler fd ripgrep zoxide tlrc"
+
+install_homebrew_packages() {
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.bashrc
+	brew install $HOMEBREW_PACKAGES
+}
+
+$install_homebrew_packages
+
 
 # Function to install ble.sh
 install_ble_sh() {
@@ -29,24 +51,35 @@ install_ble_sh() {
 	echo 'source ~/.local/share/blesh/ble.sh' >>~/.bashrc
 }
 
-# Function to pull git repo                                                                  
-install_repo() {                                                                              
-  git clone https://github.com/st3f4nh4ck/.dotfiles.git > $HOME
-  rm -rf $HOME/.bashrc
-  stow $HOME/.dotfiles .
+$install_ble_sh
 
-}                                                                                               }
+
+# Function to pull git repo
+install_repo() {
+	git clone https://github.com/st3f4nh4ck/.dotfiles.git >$HOME
+	rm -rf $HOME/.bashrc
+	stow $HOME/.dotfiles .
+}
+
+$install_repo
+
 
 # Function to install GDM Settings to change login wallpaper
 install_gdm_settings() {
 	flatpak install io.github.realmazharhussain.GdmSettings org.onlyoffice.desktopeditors org.gnome.World.PikaBackup io.gitlab.zehkira.Monophony io.github.mimbrero.WhatsAppDesktop org.chromium.Chromium org.gnome.Solanum org.qbittorrent.qBittorrent io.github.hvdwofl.jExifToolGUI com.belmoussaoui.Obfuscate net.sourceforge.Pdfedit
 }
 
+$install_gdm_settings
+
+
 # Function to install Snap Store
 install_snap_store() {
 	sudo apt install snapd -y
 	sudo snap install core && sudo snap install snap-store && sudo snap install csbooks
 }
+
+$install_snap_store
+
 
 # Function to install VirtualBox
 install_virtualbox() {
@@ -55,6 +88,8 @@ install_virtualbox() {
 	sudo apt update
 	sudo apt install virtualbox-7.0 -y
 }
+
+$install_virtualbox
 
 # Function to install VBox Guest Extension
 install_vbox_guest_extension() {
@@ -65,13 +100,10 @@ install_vbox_guest_extension() {
 	sudo usermod -aG vboxusers $USER
 }
 
-# Function to install cht.sh
-install_cht_sh() {
-	sudo curl https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh
-	sudo chmod +x /usr/local/bin/cht.sh
-}
+$install_vbox_guest_extension
 
-# Function to install themes
+
+# Function to download fonts & install themes
 install_themes() {
 	cd $HOME/Downloads
 	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Hack.zip
@@ -80,33 +112,21 @@ install_themes() {
 	git clone https://github.com/vinceliuice/Tela-icon-theme.git
 	cd Tela-icon-theme
 	./install.sh
-	cd $HOME/Downloads ; 	git clone https://github.com/vinceliuice/Orchis-theme.git
-	cd Orchis-theme ; 	./install.sh --tweaks macos
+	cd $HOME/Downloads
+	git clone https://github.com/vinceliuice/Orchis-theme.git
+	cd Orchis-theme
+	./install.sh --tweaks macos
 }
 
-# Function to remove bloatware
-remove_bloat() {
-	sudo apt purge gnome-games transmission libreoffice* -y && sudo apt autoremove -y && sudo apt autoclean -y
-}
+$install_themes
 
-# Perform installations
-remove_bloat
-install_apt_packages
-install_oh_my_bash
-install_homebrew_packages
-install_ble_sh
-install_repo
-install_flatpak
-install_gdm_settings
-install_snap_store
-install_virtualbox
-install_vbox_guest_extension
-install_cht_sh
-install_themes
 
-echo 'export PATH=$PATH:/usr/sbin:/usr/local/bin:/opt' >>$HOME/.bashrc
+#update PATH
+echo 'export PATH=$PATH:/opt' >> $HOME/.bashrc
+
+#install rust lang
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-
 sudo nala upgrade
-echo "Setup completed."
+
+figlet "The Machine is Ready."
